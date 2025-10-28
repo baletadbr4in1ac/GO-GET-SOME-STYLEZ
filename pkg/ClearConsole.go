ee050d8ab4a5321f7f4136a21b475f7372ada8a8
BraiNiac <mateusbalabenute@gmail.com>
2025-10-28 03:09:22 -0300

    feat:(ClearConsole): Simplified console clearing method

--- pkg/ClearConsole.go
+++ pkg/ClearConsole.go
@@ -1,28 +1,32 @@
 package pkg
 
 import (
-	"os"
-	"os/exec"
+	"fmt"
 )
 
-func ClearConsole() string {
-	osConsoleToClean := OSDetectionSimple()
-	switch osConsoleToClean {
-	case "linux":
-		cmd := exec.Command("clear")
-		cmd.Stdout = os.Stdout
-		err := cmd.Run()
-		if err != nil {
-			return ""
-		}
-	case "windows":
-		cmd := exec.Command("cls")
-		cmd.Stdout = os.Stdout
-		err := cmd.Run()
-		if err != nil {
-			return ""
-		}
-		return "windows"
-	}
-	return "unknown"
+func ClearConsole() {
+	fmt.Print("\033[H\033[2J")
+	return
 }
+
+//func ClearConsole() string {
+//	osConsoleToClean := OSDetectionSimple()
+//	switch osConsoleToClean {
+//	case "linux":
+//		cmd := exec.Command("clear")
+//		cmd.Stdout = os.Stdout
+//		err := cmd.Run()
+//		if err != nil {
+//			return ""
+//		}
+//	case "windows":
+//		cmd := exec.Command("cls")
+//		cmd.Stdout = os.Stdout
+//		err := cmd.Run()
+//		if err != nil {
+//			return ""
+//		}
+//		return "windows"
+//	}
+//	return "unknown"
+//}
